d32903e87639d69cc9ca3b1fbe20f8cb13c99f2a
jreue <jreue@users.noreply.github.com>
2026-01-14 16:19:24 -0600

    Now using new TimeMachineShared library

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <MessageStructs.h>
 #include <WiFi.h>
 #include <esp_now.h>
 
@@ -11,48 +12,6 @@ LEDStatusHelper ledHelper;
 DisplayController displayController;
 
 GameEngine gameEngine;
-
-// Message types
-#define MSG_TYPE_CONNECT 0
-#define MSG_TYPE_STATUS 1
-#define MSG_TYPE_DISCONNECT 2
-#define MSG_TYPE_DATE_UPDATE 3
-#define MSG_TYPE_SCANNER_CONNECTED 4
-
-// Common header for all messages
-struct EspNowHeader {
-    uint8_t id;
-    uint8_t messageType;
-};
-
-// Device message (connection/calibration status)
-struct DeviceMessage {
-    uint8_t id;
-    uint8_t messageType;
-    bool isCalibrated;
-};
-
-// Scanner message
-struct ScannerMessage {
-    uint8_t id;
-    uint8_t messageType;
-};
-
-// Date module message
-struct DateMessage {
-    uint8_t id;
-    uint8_t messageType;
-    uint8_t month;
-    uint8_t day;
-    uint16_t year;
-};
-
-// Device state tracking
-struct DeviceState {
-    bool available = false;
-    bool calibrated = false;
-    unsigned long lastSeen = 0;
-};
 
 const uint8_t KNOWN_DEVICE_IDS[] = {DEVICE_1_ID, DEVICE_2_ID, DEVICE_3_ID};
 const int NUM_DEVICES = sizeof(KNOWN_DEVICE_IDS) / sizeof(KNOWN_DEVICE_IDS[0]);
